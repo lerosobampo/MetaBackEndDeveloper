e5475e774de5acef09228d50e08bccb36423362e
lerosobampo <osobampo.luis@gmail.com>
2023-06-19 23:40:46 -0600

    Models, MySQL, Register Models, LittleLemon Database, and SuperAdmin

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Menu     #TABLE IMPORT
+from .models import Booking  #TABLE IMPORT
 
 # Register your models here.
+admin.site.register(Menu)       #TABLE REGISTER
+admin.site.register(Booking)    #TABLE REGISTER
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    inventory = models.IntegerField(5)
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    guests = models.IntegerField(6)             #No_of_guests
+    bookingDate = models.DateTimeField()
